300448c971569f31a4277a5e10e24253598450bb
Nat <nathaniel18_2@yahoo.fr>
2021-10-08 03:27:09 +0200

    draft infinite line handling

--- get_next_line.c
+++ get_next_line.c
@@ -11,27 +11,34 @@ char *get_next_line(int fd)
 {
 	static char buff[BUFFSIZE];
 	static uint ibuf = 0;
-	uint iline = 0;
-	char *line;
+	uint line_sz;
+	char *line_buf;
+	t_list *line_parts;
 	int nread;
+	char guard;
 
-	line = malloc(10000);
+	line_sz = 0;
+	line_buf = &guard; // see (1)
 	while (buff[ibuf] != '\n' && ibuf != (uint) nread)
 	{
 		if (ibuf == 0)
+		{
+			line_buf[line_sz++ % (BUFFSIZE + 1)] = '\0';
+			ft_lstadd_front(&line_parts, ft_lstnew(line_buf));
+			//TODO ext fst time (1) ^^^
 			nread = read(fd, buff, BUFFSIZE);
-		line[iline] = buff[ibuf];
-		//printf("char %u in buff : %c\n", ibuf, buff[ibuf]);
-		++iline;
-		ibuf = (ibuf + 1) % BUFFSIZE;
+			line_buf = malloc(BUFFSIZE + 1);
+		}
+		line[line_sz++ % (BUFFSIZE + 1)] = buff[ibuf++];
+		ibuf %= BUFFSIZE;
 	}
 	if (ibuf != (uint) nread)
-		++ibuf;
-	else if (iline == 0)
+		line_parts[line_sz++ % (BUFFSIZE + 1)] = buff[ibuf++];
+	line_buf[line_sz++ % (BUFFSIZE + 1)] = '\0';
+	ft_lstadd_front(line_parts, ft_lstnew(line_buf));
+	if (line_sz == 0)
 		return (NULL);
-	line[iline] = '\n';
-	line[iline + 1] = '\0';
-	return (line);
+	return (ft_merge_parts(line_parts, line_sz));
 }
 
 #include <fcntl.h>
@@ -54,6 +61,3 @@ int main(int ac, char **av)
 	}
 	return (0);
 }
-
-
-
